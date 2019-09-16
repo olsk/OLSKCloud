337031a440c06810e5d47fcf0187afebc07b9ddb
Rosano Coutinho <pub@xeari.com>
2019-09-16 17:54:33 -0400

    Scaffold svelte

---
+++ _OSWContainer/main.svelte
@@ -0,0 +1,7 @@
+<script>
+import { OLSKLocalized } from '../_shared/_common/global.js';
+</script>
+
+<div class="OSWContainer">{ OLSKLocalized('OSWAlfa') }</div>
+
+<style src="./ui-style.css"></style>
---
+++ _shared/_common/global.js
@@ -0,0 +1,5 @@
+let localizationDictionary = JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`);
+import OLSKInternational from 'OLSKInternational';
+export const OLSKLocalized = function(translationConstant) {
+	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, localizationDictionary[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
+};
