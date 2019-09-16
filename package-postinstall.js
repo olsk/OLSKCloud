d4378c5d36904984155a2c74ce35df8ed67ceca6
Rosano Coutinho <pub@xeari.com>
2019-09-16 17:39:06 -0400

    Add package.json

---
+++ package-postinstall.js
@@ -0,0 +1,8 @@
+(function OLSKPostinstallExternalAssets() {
+	const OLSKAssets = require('./node_modules/OLSKApp/modules/OLSKAssets/main.js');
+	const pathPackage = require('path');
+
+	OLSKAssets.OLSKAssetsCopyAssetsFromTo([
+		'normalize.css',
+	], pathPackage.join(__dirname, 'node_modules'), pathPackage.join(__dirname, '_shared/__external'));
+})();
