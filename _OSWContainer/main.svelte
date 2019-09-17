8cf8a7abdd60383ad4ed7fc42b0766bcdc9e026e
Rosano Coutinho <pub@xeari.com>
2019-09-17 16:34:37 -0400

    Pass dispatch

--- _OSWContainer/main.svelte
+++ _OSWContainer/main.svelte
@@ -1,14 +1,33 @@
 <script>
 export let ContainerDelegate;
+export const ContainerDispatch = function (event) {
+	mod.MessageContainerDispatch(event)
+}
 
 import { OLSKLocalized } from '../_shared/_common/global.js';
 
 import OSWStorageOptions from '../OSWStorageOptions/main.svelte'
+
+const mod = {
+
+	OSWStorageOptionsError: '',
+
+	// MESSAGE
+
+	MessageContainerDispatch (event) {
+		if (event.name !== 'DiscoveryError') {
+			return;
+		};
+
+		mod.OSWStorageOptionsError = `${ event.name } ${ event.message}`
+	},
+
+};
 </script>
 
 <div class="OSWContainer">
 
-<OSWStorageOptions on:StorageOptionsDispatchSelect={ ContainerDelegate.StorageOptionsDispatchSelect } />
+<OSWStorageOptions on:StorageOptionsDispatchSelect={ ContainerDelegate.StorageOptionsDispatchSelect } OSWStorageOptionsError={ mod.OSWStorageOptionsError } />
 
 </div>
 
