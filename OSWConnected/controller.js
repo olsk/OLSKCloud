6d628c0a4a73fcbe6d9bc151c333273dc3291089
Rosano Coutinho <pub@xeari.com>
2019-09-28 15:50:56 -0400

    Localize es

--- OSWConnected/controller.js
+++ OSWConnected/controller.js
@@ -6,7 +6,7 @@ exports.OLSKControllerRoutes = function() {
 				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'stub-view'));
 			},
 			OLSKRouteSignature: 'OSWConnectedStubRoute',
-			OLSKRouteLanguages: ['en', 'fr'],
+			OLSKRouteLanguages: ['en', 'fr', 'es'],
 			OLSKRouteIsHidden: process.env.NODE_ENV === 'production',
 		}];
 };
