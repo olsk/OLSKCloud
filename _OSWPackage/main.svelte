f8c541d6891484f4e57a24f55b1e55b2f373000a
Rosano Coutinho <pub@xeari.com>
2020-06-07 10:00:59 -0300

    Import main svelte

---
+++ _OSWPackage/main.svelte
@@ -0,0 +1,30 @@
+<script>
+export let StorageClient;
+
+const mod = {
+
+	// SETUP
+
+	SetupEverything () {
+		mod.SetupStorageWidget();
+	},
+
+	SetupStorageWidget () {
+		(new window.OLSKStorageWidget(StorageClient)).attach('OLSK_STORAGE_WIDGET').backend(document.querySelector('.OLSKAppToolbarStorageButton'));
+	},
+
+	// LIFECYCLE
+
+	LifecycleModuleWillMount() {
+		mod.SetupEverything();
+	},
+
+};
+
+import { onMount } from 'svelte';
+onMount(mod.LifecycleModuleWillMount);
+</script>
+
+<div id="OLSK_STORAGE_WIDGET"></div>
+
+<style global src="./__compiled/OLSKStorageWidget.css"></style>
