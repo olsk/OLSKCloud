825304645ae4ab72a5ca851cbeaf0b416b514940
Rosano Coutinho <pub@xeari.com>
2020-10-31 10:50:26 -0300

    Purge OLSKLivePathJoin

--- OSWConnected/controller.js
+++ OSWConnected/controller.js
@@ -3,7 +3,7 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRoutePath: '/stub/OSWConnected',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction (req, res, next) {
-				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'stub-view'));
+				return res.render(require('path').join(__dirname, 'stub-view'));
 			},
 			OLSKRouteSignature: 'OSWConnectedStubRoute',
 			OLSKRouteLanguages: ['en', 'fr', 'es'],
--- _OSWPackage/controller.js
+++ _OSWPackage/controller.js
@@ -3,7 +3,7 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRoutePath: '/stub/OSWPackage',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction (req, res, next) {
-				return res.render(req.OLSKLive.OLSKLivePathJoin(__dirname, 'ui-view'));
+				return res.render(require('path').join(__dirname, 'ui-view'));
 			},
 			OLSKRouteSignature: 'OSWPackageStubRoute',
 			OLSKRouteIsHidden: process.env.NODE_ENV === 'production',
