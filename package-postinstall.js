71b6c5e5c23e0c9ee6559b05ce7cb0c85ee44c8d
Rosano Coutinho <pub@xeari.com>
2021-02-14 12:50:09 -0300

    Purge postinstall

--- package-postinstall.js
+++
@@ -1,6 +0,0 @@
-(function OLSKPostinstallExternalAssets() {
-	require('./node_modules/OLSKExpress/modules/OLSKAssets/main.js').OLSKAssetsCopyAssetsFromTo([
-		'normalize.css',
-				'OLSKUIAssets',
-	], require('path').join(__dirname, 'node_modules'), require('path').join(__dirname, '_shared/__external'));
-})();
